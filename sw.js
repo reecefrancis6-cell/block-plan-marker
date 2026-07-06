bdd384efe3cf5367a654c6ded17bde027ffec8b2
reecefrancis6-cell <reece.francis6@gmail.com>
2026-07-06 20:28:20 +1000

    iPad share sheet for save/export, .json project files

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "bpm-v2";
+const CACHE = "bpm-v3";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
