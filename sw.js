50ab28091f1fdd75f81129650e04b070676175d2
PotatoSpark <reece.francis6@gmail.com>
2026-07-10 22:36:19 +1000

    v1.17 beta: building elements tab, copy/paste, sound meter + dBA measure, camera + site photos, PDF add-onto-plan

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "bpm-v17";
+const CACHE = "bpm-v18";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png", "./pdf.min.js", "./pdf.worker.min.js"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
