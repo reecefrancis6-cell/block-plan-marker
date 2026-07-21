bda1f8f2218c2f728d73a34c788d4d8497415ed6
PotatoSpark <reece.francis6@gmail.com>
2026-07-21 12:09:11 +1000

    Block Plan Marker v1.35.1 beta - fix: Annuals modal z-index + data-URL decode without fetch (bpm-v37)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "bpm-v36";
+const CACHE = "bpm-v37";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png", "./pdf.min.js", "./pdf.worker.min.js"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
